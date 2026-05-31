da349af8afa94ce5d89603273ae298cbd6938a35
egla24330 <egla24330@gmail.com>
2026-05-31 02:22:29 -0300

    quick update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    // This explicitly tells Vite to put production files in the 'dist' folder
+    outDir: 'dist', 
+  }
 })
